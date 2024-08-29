f06fd461c59011099a2478c0bd5cfa78aa2527ff
nlilian <nnoromlili@gmail.com>
2024-08-29 12:52:40 +0100

    initial project files

---
+++ lib/calcButton.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+
+Widget calcButton(
+  String buttonText,
+  Color buttonColor,
+  void Function()? buttonPressed,
+) {
+  return Container(
+    width: 75,
+    height: buttonText == "=" ? 150 : 70,
+    padding: const EdgeInsets.all(0),
+    child: ElevatedButton(
+      onPressed: buttonPressed,
+      style: ElevatedButton.styleFrom(
+        shape: const RoundedRectangleBorder(
+            borderRadius: BorderRadius.all(Radius.circular(20))),
+        backgroundColor: buttonColor,
+      ),
+      child: Text(
+        buttonText,
+        style:  TextStyle(
+          fontSize: buttonText == "AC" || buttonText == "+/-" ? 18 : 27,
+          color: Colors.white,
+        ),
+      ),
+    ),
+  );
+}
---
+++ lib/main.dart
@@ -0,0 +1,25 @@
+import 'package:calculator/calculatorview.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const Calculator());
+}
+
+class Calculator extends StatelessWidget {
+  const Calculator({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Calculator',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      home: const CalculatorView(),
+    );
+  }
+}
+
